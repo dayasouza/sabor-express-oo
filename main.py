8bf451cb6511103f11793ba8135ea86c17224824
Daya Souza <dayanalvesouza@gmail.com>
2024-03-23 16:47:38 -0300

    feita a consulta do endpoint

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ app = FastAPI()
 
 @app.get('/api/hello')
 def hello_world():
+    """endpoint que exibe uma imagem incrivel do mundo da programação!
+    """
     return {'Hello': 'World'}
 
 @app.get('/api/restaurantes/')
 def get_restaurantes(restaurante: str = Query(None)):
+    """Endpoint para ver os cardapios dos restaurantes
+
+    Args:
+        restaurante (str, optional): _description_. Defaults to Query(None).
+
+    """
     url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
     response = requests.get(url)
     print(response)
